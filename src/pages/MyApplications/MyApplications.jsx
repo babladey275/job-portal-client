50d55b51bc7a4418aaf2af317ecfe4d0e3aacfb9
babladey275 <babladey275@gmail.com>
2024-12-22 18:22:11 +0600

    added axios interceptors

--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -3,20 +3,26 @@ import useAuth from "../../hooks/useAuth";
 import { FaTrash } from "react-icons/fa";
 import Swal from "sweetalert2";
 import axios from "axios";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const MyApplications = () => {
   const { user } = useAuth();
   const [jobs, setJobs] = useState([]);
+  const axiosSecure = useAxiosSecure();
 
   useEffect(() => {
     // fetch(`http://localhost:5000/job-application?email=${user.email}`)
     //   .then((res) => res.json())
     //   .then((data) => setJobs(data));
 
-    axios
-      .get(`http://localhost:5000/job-application?email=${user.email}`, {
-        withCredentials: true,
-      })
+    // axios
+    //   .get(`http://localhost:5000/job-application?email=${user.email}`, {
+    //     withCredentials: true,
+    //   })
+    //   .then((res) => setJobs(res.data));
+
+    axiosSecure
+      .get(`/job-application?email=${user.email}`)
       .then((res) => setJobs(res.data));
   }, [user.email]);
 
